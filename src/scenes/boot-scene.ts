ee7898de19483cd63a9464769a5ead9a75cec62b
Carli Nabbe <carli_nabbe@hotmail.com>
2019-05-29 11:04:55 +0200

    added level background and RoboAnimal

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -20,8 +20,9 @@ export class BootScene extends Phaser.Scene {
         this.load.image('ground', require('../assets/platform_ground.png'))
         this.load.image('start', require('../assets/videogame.png'))
         this.load.image('cat', require('../assets/kittycat.png'))
-        this.load.image('city', require('../assets/background_2.png'))
+        this.load.image('level_ice', require('../assets/level_ice.png'))
         this.load.image('neko', require('../assets/N-3KO_IDLE1.png'))
+        this.load.image('raElephant', require('../assets/RoboAnimals/raElephant.png'))
 
         this.load.on('complete', () => {
             console.log("everything is loaded")
--- src/scenes/next-scene.ts
+++ src/scenes/next-scene.ts
@@ -10,6 +10,13 @@ export class NextScene extends Phaser.Scene {
     private collectedScraps = 0
     private scoreField 
 
+    // Collect a roboAnimal
+    private roboAnimal = {
+        collected: false
+    }
+
+    private imageArray
+
     constructor() {
         super({ key: "NextScene" })
     }
@@ -19,7 +26,8 @@ export class NextScene extends Phaser.Scene {
     }
 
     create(): void {
-        this.add.image(0, 0, 'city').setOrigin(0, 0)      
+        this.add.image(0, 0, 'level_ice').setOrigin(0, 0)
+         
     
         // 11 STARS
         this.scraps = this.physics.add.group({
@@ -33,14 +41,15 @@ export class NextScene extends Phaser.Scene {
 
         this.platforms = this.add.group({ runChildUpdate: true })
         this.platforms.addMultiple([
-            new Platform(this, 800, 574, "ground"),
+            new Platform(this, 800, 870, "ground"),
             new Platform(this, 200, 150, "ice"),
             new MovingPlatform(this, 300, 300, "platform"),
             new Platform(this, 500, 200, "platform"),
             new MovingPlatform(this, 600, 400, "platform")
         ], true)
 
-        this.scoreField = this.add.text(200, 20,  + this.collectedScraps+ ' STARS COLLECTED', { fontFamily: 'Arial Black', fontSize: 20, color: '#000000' }).setOrigin(0.5).setStroke('#2ac9be', 2)
+        this.scoreField = this.add.text(200, 20,  + this.collectedScraps+ ' SCRAPS COLLECTED',
+        { fontFamily: 'Arial Black', fontSize: 20, color: '#000000' }).setOrigin(0.5).setStroke('#2ac9be', 2)
         
         // define collisions for bouncing, and overlaps for pickups
         this.physics.add.collider(this.scraps, this.platforms)
@@ -55,10 +64,14 @@ export class NextScene extends Phaser.Scene {
         this.collectedScraps++
 
         // TO DO check if we have all the scraps, then go to the end scene
-        this.scoreField.text = this.collectedScraps+ ' STARS COLLECTED'
+        this.scoreField.text = this.collectedScraps+ ' SCRAPS COLLECTED'
         
-        if(this.collectedScraps == 12){
-            this.scene.start('NextScene')
+        if(this.collectedScraps == 1){
+
+            this.add.image(0, 0, 'raElephant').setOrigin(0, 0)
+            this.roboAnimal.collected = true;
+            console.log(`De RoboAnimal is nu ${this.roboAnimal.collected}`)
+            // this.scene.start('NextScene')
         }
     }
 
