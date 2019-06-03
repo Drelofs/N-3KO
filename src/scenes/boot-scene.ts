54342a22ce4847ce691214d81797bf4fe7901342
Ali Shahid <ali.shahid.1994@gmail.com>
2019-06-03 16:13:35 +0200

    building boot screen and docs

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -1,3 +1,5 @@
+import { Game } from "phaser";
+
 export class BootScene extends Phaser.Scene {
 
     private graphics: Phaser.GameObjects.Graphics
@@ -7,6 +9,12 @@ export class BootScene extends Phaser.Scene {
     }
 
     init(){
+    }
+
+    private startGame(){
+        console.log("Game begins")
+        console.log(this)
+        this.scene.start("StartScene")
     }
 
     preload(): void {
@@ -32,14 +40,18 @@ export class BootScene extends Phaser.Scene {
         
         
         // N-3KO Sprites
+
+        //Idle sprites
         this.load.image('NEKO_IDLE1', require('../assets/N-3KO_IDLE1.png'))
         this.load.image('NEKO_IDLE2', require('../assets/N-3KO_IDLE2.png'))
 
+        //Walking sprites
         this.load.image('NEKO_WALK1', require('../assets/N-3KO_WALK1.png'))
         this.load.image('NEKO_WALK2', require('../assets/N-3KO_WALK2.png'))
         this.load.image('NEKO_WALK3', require('../assets/N-3KO_WALK3.png'))
         this.load.image('NEKO_WALK4', require('../assets/N-3KO_WALK4.png'))
 
+        //Running sprites
         this.load.image('NEKO_RUN1', require('../assets/N-3KO_RUN1.png'))
         this.load.image('NEKO_RUN2', require('../assets/N-3KO_RUN2.png'))
         this.load.image('NEKO_RUN3', require('../assets/N-3KO_RUN3.png'))
@@ -47,6 +59,7 @@ export class BootScene extends Phaser.Scene {
         this.load.image('NEKO_RUN5', require('../assets/N-3KO_RUN5.png'))
         this.load.image('NEKO_RUN6', require('../assets/N-3KO_RUN6.png'))
 
+        //Jumping sprites
         this.load.image('NEKO_JUMP1', require('../assets/N-3KO_JUMP1.png'))
         this.load.image('NEKO_JUMP2', require('../assets/N-3KO_JUMP2.png'))
         this.load.image('NEKO_JUMP3', require('../assets/N-3KO_JUMP3.png'))
@@ -58,10 +71,7 @@ export class BootScene extends Phaser.Scene {
         
 
         this.load.on('complete', () => {
-            console.log("everything is loaded")
-            
-            // add code here to switch to the start scene
-            this.scene.start("StartScene")
+            let timer = this.time.delayedCall(2000, this.startGame, null, this);
         })
     }
 }
