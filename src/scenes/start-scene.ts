b1550f5903c25a2e9e33dbc9197fe9668252ab61
Julia <julialanu@hotmail.com>
2019-06-24 15:48:50 +0200

    Startscreen change

--- src/scenes/start-scene.ts
+++ src/scenes/start-scene.ts
@@ -23,17 +23,11 @@ export class StartScene extends Phaser.Scene {
         this.arcade = g.arcade
 
 
-        this.add.image(0, 0, 'WASTELAND1').setOrigin(0, 0)
+        this.add.image(0, 0, 'startscreen').setOrigin(0, 0)
 
-        // add another image here
-        this.add.image(615, 500, 'NEKO_IDLE1').setOrigin(0, 0)
-
-        // add text here
-        this.add.text(700, 300, 'N-3KO', { fontFamily: 'Arial Black', fontSize: 70, color: '#2ac9be' }).setOrigin(0.5).setStroke('#7df2ea', 16)
-        this.add.text(700, 370, 'The scrap collector', { fontFamily: 'Arial Black', fontSize: 40, color: '#2ac9be' }).setOrigin(0.5).setStroke('#7df2ea', 16)
 
         // add code here to switch to the GameScene, after a mouse click
-        let btn1 = this.add.text(700,450, 'start', { fontFamily: 'Arial Black', fontSize: 60, color: '#2ac9be' }).setOrigin(0.5).setStroke('#7df2ea', 16)
+        let btn1 = this.add.text(600,850, 'start', { fontFamily: 'Arial Black', fontSize: 40, color: '#ffffff' }).setOrigin(0.5).setStroke('#ffff0000', 16)
         btn1.setInteractive()
         btn1.on('pointerdown', (pointer) => {
             this.scene.start('GameScene')
@@ -44,7 +38,7 @@ export class StartScene extends Phaser.Scene {
             this.scene.start('GameScene')
         })
 
-        let btn2 = this.add.image(100, 100, 'trophy')
+        let btn2 = this.add.image(100, 800, 'trophy')
         btn2.setInteractive()
         btn2.on('pointerdown', (pointer) => {
             this.scene.start('Collectibles')
