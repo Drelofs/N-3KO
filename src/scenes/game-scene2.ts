941e490bf4a1b51a2b373f49f556db77a39d7c0a
Julia <julialanu@hotmail.com>
2019-06-26 09:43:25 +0200

    GameScene2 aangemaakt xx

--- src/scenes/game-scene2.ts
+++ src/scenes/game-scene2.ts
@@ -1,79 +1,128 @@
 import { Player } from "../objects/player"
 import { enemy } from "../objects/bomb"
+import { Arcade } from "../arcade/arcade"
+import { Bullet } from "../objects/bullet"
 import { Platform } from "../objects/platform"
-import { MovingPlatform } from "../objects/movingplatform"
+import { threadId } from "worker_threads";
+import { Neko } from "../app"
 
 export class GameScene2 extends Phaser.Scene {
 
+    private arcade : Arcade
+    private joystickListener: EventListener
     private player : Player
-    private platforms: Phaser.GameObjects.Group
+    private hills: Phaser.GameObjects.Group
+    private platform: Phaser.GameObjects.Group
     private scraps: Phaser.Physics.Arcade.Group
-    private collectedScraps = 0
+    private bulletGroup: Phaser.GameObjects.Group
     private scoreField
     private enemies: Phaser.GameObjects.Group
-
-    private lives = 9
+    private bgtile: Phaser.GameObjects.TileSprite
+    private lives = 2
     private livesField
 
     private timer : Phaser.Time.TimerEvent
     private hitTimeout = false
-
-    // Collect a roboAnimal
-    private roboAnimal = {
-        collected: false
-    }
-
-    private imageArray
+ 
 
     constructor() {
         super({ key: "GameScene2" })
+     
+    }
+
+    public friendlyBullet() {
+        this.bulletGroup.add(new Bullet(this, this.player.x+20,  this.player.y, this.player.direction), true)
+        let config = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0
+        }
+        let laser = this.scene.scene.sound.add('laser', config);
+        laser.play();
+        
     }
 
     init(): void {
-        console.log("dit is de GameScene2")
+        let g = this.game as Neko
+        this.arcade = g.arcade
+        
     }
 
     create(): void {
-        this.add.image(0, 0, 'level_ice').setOrigin(0, 0)
-         
-    
-        // 11 STARS
+        this.bgtile = this.add.tileSprite(0, 0, 1800, 900, 'WASTELAND1')
+        this.bgtile.setOrigin(0,0)
+
+        this.bulletGroup = this.add.group({ runChildUpdate: true})
+
+        let config = {
+            mute: false,
+            volume: 20,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0
+        }
+        let music = this.scene.scene.sound.add('meow', config);
+        music.play();
+        
+        
+       
+        // 11 SCRAPS
         this.scraps = this.physics.add.group({
             key: 'scrap',
-            repeat: 11,
+            repeat: 12,
             setXY: { x: 12, y: 30, stepX: 70 },
         })
 
         this.enemies = this.add.group()
-        for (let i =0; i <1; i++){
-            this.enemies.add(new enemy(this, 700, 255), true)
+        for (let i =0; i <4; i++){
+            this.enemies.add(new enemy(this, 250*i+250, 255), true)
         }
 
-        // TODO add player
+        // TODO add player and enemy
+        
         this.player = new Player(this)
 
-        this.platforms = this.add.group({ runChildUpdate: true })
-        this.platforms.addMultiple([
-            new Platform(this, 800, 870, "ground"),
-            // new Platform(this, 400, 300, "HILL1"),
-            // new Platform(this, 200, 150, "ice"),
-            // new MovingPlatform(this, 300, 300, "platform"),
-            // new Platform(this, 500, 200, "platform"),
-            // new MovingPlatform(this, 600, 400, "platform")
-        ], true)
 
-        this.scoreField = this.add.text(200, 20,  + this.collectedScraps+ ' SCRAPS COLLECTED',
-        { fontFamily: 'Arial Black', fontSize: 20, color: '#000000' }).setOrigin(0.5).setStroke('#2ac9be', 2)
+        this.platform = this.add.group({ runChildUpdate: true})
+        this.hills = this.add.group({ runChildUpdate: true })
+
+        //PLATFORM  
+        // const platform : Platform = (this.platform.children.entries[0]) as Platform
+        this.physics.add.collider(this.player, this.platform)
+        this.platform.add(new Platform(this, 300, 800, 'FLAT1'), true)
+        this.platform.add(new Platform(this, 900, 800, 'MEDIUM1'), true)
+        this.platform.add(new Platform(this, 400, 400, 'AIR1'), true)
+        this.platform.add(new Platform(this, 1300, 250, 'AIR2'), true)
+
 
         this.livesField = this.add.text(1340, 20,  + this.lives+ ' LIVES LEFT', { fontFamily: 'Arial Black', fontSize: 20, color: '#000000' }).setOrigin(0.5).setStroke('#FFFFFF', 2)
-        
+
         // define collisions for bouncing, and overlaps for pickups
-        this.physics.add.collider(this.scraps, this.platforms)
-        this.physics.add.collider(this.player, this.platforms)
-        this.physics.add.collider(this.enemies, this.platforms)
+        this.physics.add.collider(this.scraps, this.platform)
+        this.physics.add.collider(this.player, this.platform)
+        this.physics.add.collider(this.enemies, this.platform)
         
         this.physics.add.overlap(this.player, this.scraps, this.collectScraps, null, this)
         this.physics.add.overlap(this.player, this.enemies, this.hitEnemy, null, this)
+
+
+        this.physics.add.overlap(this.bulletGroup, this.enemies, this.killEnemy, null, this)
+
+        this.physics.world.bounds.width = 1800
+        this.physics.world.bounds.height = 900
+
+        // this.cameras.main.setSize(1440,900) //canvas
+        this.cameras.main.setBounds(0,0,1800,900) //game
+        this.cameras.main.startFollow(this.player)
+
+        this.player.setCollideWorldBounds(true)
+
     }
 
     private setTimer() {
@@ -96,7 +145,6 @@ export class GameScene2 extends Phaser.Scene {
                 ease: 'Sine.easeInOut',
                 duration: 200,
                 alpha: 0.4,
-                color: 0xff0000,
                 yoyo:true,
                 repeat:4
             })
@@ -104,31 +152,50 @@ export class GameScene2 extends Phaser.Scene {
         
         // Game over. Reset scraps & Lives
         if (this.lives === 0) {
-            this.collectedScraps = 0;
+            this.registry.values.scraps = 0;
             this.scene.start("EndScene")
-            this.lives = 9
+            this.lives = 2
         }
     }
 
-    private collectScraps(player : Player , scrap) : void {
-        this.scraps.remove(scrap, true, true)
+    private killEnemy(bullet: Bullet, enemy : enemy) {
+        console.log("enemy geraakt!")
+        console.log(bullet)
+        console.log(enemy)
+        this.enemies.remove(enemy, true, true)
+    }
+
+
+    private collectScraps(player : Player , scraps) : void {
+        this.scraps.remove(scraps, true, true)
         // this.registry.values.score++
-        this.collectedScraps++
+        this.registry.values.scraps++
 
-        // TO DO check if we have all the scraps, then go to the end scene
-        this.scoreField.text = this.collectedScraps+ ' SCRAPS COLLECTED'
-
-        if(this.collectedScraps == 7){
-            // this.scene.start('GameScene2')
-           this.collectedScraps = 0
-
-            this.add.image(900, 0, 'raElephant').setOrigin(0, 0);
-            this.roboAnimal.collected = true;
-            this.add.text(870, 200, 'You earned a RoboAnimal!')
+        let config = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0
+        }
+        let bell = this.scene.scene.sound.add('bell', config);
+        bell.play();
+        
+        
+        // TO DO check if we have all the stars, then go to the end scene
+        // this.scoreField.text = this.registry.values.scraps+ ' SCRAPS COLLECTED'
+        
+        if(this.registry.values.scraps == 12){
+            this.scene.start('GameScene2')
+           this.registry.values.scraps = 0
         }
     }
     update(){
         this.player.update()
+        // this.bgtile.tilePositionX += 1
+
     }
 
 }
